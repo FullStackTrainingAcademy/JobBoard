d8d75ed024dff7732173ccfbe88b9b9e06daa63b
United States CA <jamalhoseen@Jamal-Hoseens-Air.local>
2022-05-16 21:24:16 -0700

    chicken

---
+++ script.js
@@ -0,0 +1,13 @@
+// $.ajax({
+//     url: "https://random-words-api.vercel.app/word",
+//     method: "GET",
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error: function (error) {
+//         console.log(`Error`);
+//     }
+// });
+
+console.log('chicken')
+
